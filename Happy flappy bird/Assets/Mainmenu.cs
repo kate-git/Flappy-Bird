b3a84464ec8a15b72ff126526e582e5e5a02e839
airikuu <aerieluna.ameen@hyperisland.se>
2025-01-14 15:39:18 +0100

    menu button script

--- Happy flappy bird/Assets/Mainmenu.cs
+++ Happy flappy bird/Assets/Mainmenu.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuController : MonoBehaviour
+
+public class MainMenuManager : MonoBehaviour
 {
-    public void StartBtn()
+    public void StartGame()
     {
+   
         SceneManager.LoadScene("TestEnvironment");
     }
+
 }
